fb98eb40de804da36fc393549cdabfee1e1289bf
Njato <herilazanjato@gmail.com>
2023-11-28 10:31:07 +0300

    responsive svg sur mobil

--- src/Assets/svg/ExagonComp2.tsx
+++ src/Assets/svg/ExagonComp2.tsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import './style/styleIcon.scss'
+
 
 type Props = {
     className: string
@@ -7,7 +9,7 @@ type Props = {
 const ExagonComp2 = ({ className }: Props) => {
     return (
         <div className={className}>
-            <svg width="249" height="263" viewBox="0 0 249 263" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <svg className='resExagComp2' width="249" height="263" viewBox="0 0 249 263" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path d="M0.667961 133.951L189.408 258.982L249 263L249 37.5L149.567 0.486331L98.2374 49.7698L0.667961 133.951Z" fill="#4A8B58" />
             </svg>
 
--- src/Assets/svg/ExagonCont1.tsx
+++ src/Assets/svg/ExagonCont1.tsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import './style/styleIcon.scss'
+
 
 type Props = {
     className: string
@@ -7,7 +9,7 @@ type Props = {
 const ExagonCont1 = ({ className }: Props) => {
     return (
         <div className={className}>
-            <svg width="232" height="453" viewBox="0 0 232 453" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <svg className='resExagCont1' width="232" height="453" viewBox="0 0 232 453" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path d="M231.021 345L14.5215 453L0.0214844 422L3.05176e-05 3L136.521 0L168.021 125L231.021 345Z" fill="#4A8B58" />
             </svg>
 
